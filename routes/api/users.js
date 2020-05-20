1cf5774c2c3ebf6567993a24d2e75ae1ffc9a2e5
Peter Kim <ksi9302@gmail.com>
2020-05-20 15:56:35 +1000

    User login added

--- routes/api/users.js
+++ routes/api/users.js
@@ -72,9 +72,10 @@ router.post(
           res.json({ token });
         }
       );
-    } catch (err) {}
-    console.error(err.message);
-    res.status(500).send("Server error");
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send("Server error");
+    }
   }
 );
 
