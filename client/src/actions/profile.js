15a2d9e266a5851fbd2c34d6b208facec2d139a4
Peter K <ksi9302@gmail.com>
2020-06-02 21:34:18 +1000

    Prepared for deployment

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -52,7 +52,7 @@ export const getProfiles = () => async dispatch => {
 // Get profile by ID
 export const getProfileById = userId => async dispatch => {
   try {
-    const res = await axios.get(`/api/profile/${userId}`);
+    const res = await axios.get(`/api/profile/user/${userId}`);
     //set Auth token already should have set the default header for axios from Auth process
 
     dispatch({
--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -75,8 +75,8 @@ const EditProfile = ({
 
   return (
     <>
-      <h1 classNameName="large text-primary">Create Your Profile</h1>
-      <p classNameName="lead">
+      <h1 className="large text-primary">Create Your Profile</h1>
+      <p className="lead">
         <i className="fas fa-user"></i> Let's get some information to make your
         profile stand out
       </p>
